ebebe87025a095d2e933afe5a7799bb455d780eb
michaelbacki <mdkulbac@buffalo.edu>
2019-05-01 22:13:17 -0400

    First pass at QR Code Recognition

--- wrappers/python/examples/opencv_viewer_example.py
+++ wrappers/python/examples/opencv_viewer_example.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 ## License: Apache 2.0. See LICENSE file in root directory.
 ## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
 
@@ -8,15 +9,18 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import pyzbar.pyzbar as pyzbar
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 
 # Start streaming
 pipeline.start(config)
+
+font = cv2.FONT_HERSHEY_PLAIN
 
 try:
     while True:
@@ -28,19 +32,44 @@ try:
         if not depth_frame or not color_frame:
             continue
 
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
+        width = depth_frame.get_width();
+        height = depth_frame.get_height();
+        dist = depth_frame.get_distance(0, 0)
+        #print (dist);
+
         color_image = np.asanyarray(color_frame.get_data())
+        decodedObjects = pyzbar.decode(color_image)
+        for barcode in decodedObjects:
+            print("Data", barcode.data)
+            # extract the bounding box location of the barcode and draw
+    		# the bounding box surrounding the barcode on the image
+            (x, y, w, h) = barcode.rect
+            cv2.rectangle(color_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    		# the barcode data is a bytes object so if we want to draw it
+    		# on our output image we need to convert it to a string first
+            barcodeData = barcode.data.decode("utf-8")
+            barcodeType = barcode.type
+
+    		# draw the barcode data and barcode type on the image
+            text = "{} ({})".format(barcodeData, barcodeType)
+            cv2.putText(color_image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+        cv2.imshow("QR Detector Frame", color_image)
+
+        # Convert images to numpy arrays
+        #depth_image = np.asanyarray(depth_frame.get_data())
+        #color_image = np.asanyarray(color_frame.get_data())
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
         # Stack both images horizontally
-        images = np.hstack((color_image, depth_colormap))
+        # images = np.hstack((color_image, depth_colormap))
 
         # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
+        #cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        #cv2.imshow('RealSense', color_image)
         cv2.waitKey(1)
 
 finally:
