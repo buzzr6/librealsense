7586b7923cc639917af8c67a7983da1084ee85fa
michaelbacki <mdkulbac@buffalo.edu>
2019-05-12 18:56:48 -0400

    added center guiding lines, pixel location is now center of code, removed old code

--- wrappers/python/examples/qr_code_recognition.py
+++ wrappers/python/examples/qr_code_recognition.py
@@ -15,8 +15,32 @@ import socket
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
+video_height = 540
+video_width = 960
 config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 60)
+config.enable_stream(rs.stream.color, video_width, video_height, rs.format.bgr8, 60)
+
+center_x_low = video_width/2 - 75
+center_x_high = video_width/2 + 75
+x_axis = video_width/2
+y_axis = video_height/2
+
+def barcode_location(x, y):
+    if (x < center_x_high) * ( x > center_x_low):
+        return "middle"
+    if x < video_width/2:
+        if y < video_height/2:
+            return "top left"
+    if x < video_width/2:
+        if y > video_height/2:
+            return "bottom left"
+    if x > video_width/2:
+        if y < video_height/2:
+            return "top right"
+    if x > video_width/2:
+        if y > video_height/2:
+            return "bottom right"
+    return "unknown or center"
 
 #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.connect(('192.168.8.1', 8554))
@@ -43,24 +67,13 @@ try:
 
         color_video = np.asanyarray(color_frame.get_data())
 
-        # Create alignment primitive with color as its target stream:
-        align = rs.align(rs.stream.color)
-        aligned_frames = align.process(frames)
-
-        # Update color and depth frames:
-        aligned_depth_frame = aligned_frames.get_depth_frame()
-        depth = np.asanyarray(aligned_depth_frame.get_data())
-
-        # Get data scale from the device and convert to meters
-        #depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
-        #depth = depth * depth_scale
-        #dist,_,_,_ = cv2.mean(depth)
-        #distance = str(round(dist, 3))
-        #distance_away = distance + ' meters away'
-
         decodedObjects = pyzbar.decode(color_video)
         for barcode in decodedObjects:
             points = barcode.polygon
+
+            # Draw a center vertical and horizontal lines
+            #cv2.line(color_video,(x_axis, video_height),(x_axis, 0),(255,0,0),1)
+            #cv2.line(color_video,(0, y_axis),(video_width, y_axis),(255,0,0),1)
 
             # If the points do not form a quad, find convex hull
             if len(points) > 4 :
@@ -83,29 +96,37 @@ try:
 
     		# Draw the barcode data and the distance on the video
             (x, y, w, h) = barcode.rect
-            xy_depth = round(depth_frame.get_distance(x, y), 4)
-            print('DEPTH: ' + str(xy_depth) + "  |  X: " + str(round(x)) +  " Y: " + str(round(y)))
-            # TODO based on the pixel coords we can determine if we need to go left or right
-            # also can make sure QR is centered by knowing the pixels it should be in the range of to be centered
+            rect_center_x = (x + (x+w))/2
+            rect_center_y = (y + (y+h))/2
+            #xy_depth = round(depth_frame.get_distance(x, y), 3)
+            print('X: ' + str(round(rect_center_x)) +  "   Y: " + str(round(rect_center_y)))
+
             if w != 0:
-                distance = ((580 * 6)/w) + 1
+                distance = round(((489 * 5.25)/w) -3, 1)  # the -3 compensates for the angle
                 # We know we can detect a code further than this distance so any
                 # number higher is due to error
-                if distance > 50 :
+                if distance > 90 :
                     distance_away = "-- inches away"
                 else:
                     distance_away = str(distance) + ' inches away'
-            # TODO use the x, y pixel coords to get distance away field calculation
 
-            barcode_text = ("{} ({})".format(barcode_data, barcode_type))[:20] + '...'
+            barcode_text = ("{} ({})".format(barcode_data, barcode_type))[:2]
             cv2.putText(color_video, distance_away, (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
             cv2.putText(color_video, barcode_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            cv2.circle(color_video, (rect_center_x, rect_center_y), 7, (255, 0, 0), -1)
             #cv2.putText(color_video, distance_away, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 3)
 
+            if barcode_location(rect_center_x ,rect_center_y) == "middle":
+                # Draw estimated center for rover approach
+                cv2.line(color_video,(center_x_low, video_height),(center_x_low, 0),(0,255,0),2)
+                cv2.line(color_video,(center_x_high, video_height),(center_x_high, 0),(0,255,0),2)
+                # TODO This means barcode is inc enter range, send command
+                # to keep going straight
+
         cv2.imshow("QR Code Detection Demo", color_video)
-        #s.sendall(pickle.dumps(color_frame))
-        # TODO use distance var in payload situation
+        #s.sendall(pickle.dumps(color_video))
         cv2.waitKey(1)
+
 
 finally:
 
